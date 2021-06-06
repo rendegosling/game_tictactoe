a1aafa75f8b89aa2388b92f8b0852b1c7e0fcf7c
Ren <renlesterdeguzman@gmail.com>
2021-06-06 22:00:26 +0800

    add game pseuedo code

--- game_tictactoe.rb
+++ game_tictactoe.rb
@@ -1,9 +1,26 @@
 class GameTicTacToe
+
+  def def initialize
+    #setup board
+    #setup players
+    #assign starting player
+  end
+
   def play
-    # play the game
+    # make a loop
+    # render board
+    # get player coordinates
+    # check if game is over
+    # switch player
     puts "Game start!"
     puts "implement the game"
   end
+end
+
+class Player
+end
+
+class Board
 end
 
 game = GameTicTacToe.new
